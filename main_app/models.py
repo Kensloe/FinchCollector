cafb829ee3b8177192d9dc45c0ba9520ce2ff210
username <youremail@domain.com>
2022-05-11 14:38:29 -0400

    latest codes

--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,22 @@ class Finch(models.Model):
     #new code below
     def __str__(self):
      return f'{self.name} ({self.id})'
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
